55a3048463c4355bbea666959d0bfc45ffd64bc8
Carlos Assunção <lucasias85@gmail.com>
2024-01-12 01:29:29 -0300

    Add project files.

---
+++ Code/DAL/AcessarBancoDados.cs
@@ -0,0 +1,62 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Login_MySQL.Code.DAL
+{
+    class AcessarBancoDados
+    {
+        private MySqlConnection conn;
+
+        private string myServer = "";
+        private string myUser = "";
+        private string myPass = "";
+        private string myDatabase = "";
+
+        public void Conectar()
+        {
+            if (conn != null)
+                conn.Close();
+
+            string connStr = $"server={myServer}; uid={myUser}; pwd={myPass}; database={myDatabase}";
+
+            try
+            {
+                conn = new MySqlConnection(connStr);
+                conn.Open();
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Não foi possível conectar ao servidor de banco de dados: " + ex.Message);
+            }
+        }
+
+        public void ExecutarComandoSQL(string comandoSQL)
+        {
+            MySqlCommand cmd = new MySqlCommand(comandoSQL, conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public DataTable BuscarDataTable(string comandoSQL)
+        {
+            DataTable dataTable = new DataTable();
+            MySqlDataAdapter dataAdapter = new MySqlDataAdapter(comandoSQL, conn);
+            dataAdapter.Fill(dataTable);
+
+            return dataTable;
+        }
+
+        public MySqlDataReader BuscarDataReader(string comandoSQL)
+        {
+            MySqlCommand cmd = new MySqlCommand(comandoSQL, conn);
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+
+            return dataReader;
+        }
+    }
+}
---
+++ Code/GUI/FormLogin.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace Projeto_Login_MySQL
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelLateral = new System.Windows.Forms.Panel();
+            this.labelTitulo = new System.Windows.Forms.Label();
+            this.labelNomeUsuario = new System.Windows.Forms.Label();
+            this.textBoxNomeUsuario = new System.Windows.Forms.TextBox();
+            this.textBoxSenha = new System.Windows.Forms.TextBox();
+            this.labelSenha = new System.Windows.Forms.Label();
+            this.buttonEntrar = new System.Windows.Forms.Button();
+            this.buttonSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panelLateral
+            // 
+            this.panelLateral.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(82)))), ((int)(((byte)(219)))));
+            this.panelLateral.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLateral.Location = new System.Drawing.Point(0, 0);
+            this.panelLateral.Name = "panelLateral";
+            this.panelLateral.Size = new System.Drawing.Size(240, 360);
+            this.panelLateral.TabIndex = 0;
+            // 
+            // labelTitulo
+            // 
+            this.labelTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelTitulo.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitulo.Location = new System.Drawing.Point(240, 0);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Size = new System.Drawing.Size(400, 50);
+            this.labelTitulo.TabIndex = 1;
+            this.labelTitulo.Text = "Projeto Login MySQL";
+            this.labelTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelNomeUsuario
+            // 
+            this.labelNomeUsuario.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNomeUsuario.Location = new System.Drawing.Point(290, 80);
+            this.labelNomeUsuario.Name = "labelNomeUsuario";
+            this.labelNomeUsuario.Size = new System.Drawing.Size(300, 29);
+            this.labelNomeUsuario.TabIndex = 2;
+            this.labelNomeUsuario.Text = "Nome de usuário :";
+            this.labelNomeUsuario.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // textBoxNomeUsuario
+            // 
+            this.textBoxNomeUsuario.Location = new System.Drawing.Point(290, 110);
+            this.textBoxNomeUsuario.MaxLength = 30;
+            this.textBoxNomeUsuario.Name = "textBoxNomeUsuario";
+            this.textBoxNomeUsuario.Size = new System.Drawing.Size(300, 29);
+            this.textBoxNomeUsuario.TabIndex = 3;
+            this.textBoxNomeUsuario.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBoxSenha
+            // 
+            this.textBoxSenha.Location = new System.Drawing.Point(290, 190);
+            this.textBoxSenha.MaxLength = 30;
+            this.textBoxSenha.Name = "textBoxSenha";
+            this.textBoxSenha.PasswordChar = '*';
+            this.textBoxSenha.Size = new System.Drawing.Size(300, 29);
+            this.textBoxSenha.TabIndex = 5;
+            this.textBoxSenha.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // labelSenha
+            // 
+            this.labelSenha.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSenha.Location = new System.Drawing.Point(290, 160);
+            this.labelSenha.Name = "labelSenha";
+            this.labelSenha.Size = new System.Drawing.Size(300, 29);
+            this.labelSenha.TabIndex = 4;
+            this.labelSenha.Text = "Senha :";
+            this.labelSenha.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // buttonEntrar
+            // 
+            this.buttonEntrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(82)))), ((int)(((byte)(219)))));
+            this.buttonEntrar.FlatAppearance.BorderSize = 0;
+            this.buttonEntrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonEntrar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEntrar.ForeColor = System.Drawing.Color.White;
+            this.buttonEntrar.Location = new System.Drawing.Point(340, 250);
+            this.buttonEntrar.Name = "buttonEntrar";
+            this.buttonEntrar.Size = new System.Drawing.Size(200, 50);
+            this.buttonEntrar.TabIndex = 6;
+            this.buttonEntrar.Text = "Acessar Sistema";
+            this.buttonEntrar.UseVisualStyleBackColor = false;
+            // 
+            // buttonSair
+            // 
+            this.buttonSair.FlatAppearance.BorderSize = 0;
+            this.buttonSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSair.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(82)))), ((int)(((byte)(219)))));
+            this.buttonSair.Location = new System.Drawing.Point(395, 310);
+            this.buttonSair.Name = "buttonSair";
+            this.buttonSair.Size = new System.Drawing.Size(90, 30);
+            this.buttonSair.TabIndex = 7;
+            this.buttonSair.Text = "Sair";
+            this.buttonSair.UseVisualStyleBackColor = true;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(640, 360);
+            this.Controls.Add(this.buttonSair);
+            this.Controls.Add(this.buttonEntrar);
+            this.Controls.Add(this.textBoxSenha);
+            this.Controls.Add(this.labelSenha);
+            this.Controls.Add(this.textBoxNomeUsuario);
+            this.Controls.Add(this.labelNomeUsuario);
+            this.Controls.Add(this.labelTitulo);
+            this.Controls.Add(this.panelLateral);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FormLogin";
+            this.Opacity = 0.9D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormLogin";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelLateral;
+        private System.Windows.Forms.Label labelTitulo;
+        private System.Windows.Forms.Label labelNomeUsuario;
+        private System.Windows.Forms.TextBox textBoxNomeUsuario;
+        private System.Windows.Forms.TextBox textBoxSenha;
+        private System.Windows.Forms.Label labelSenha;
+        private System.Windows.Forms.Button buttonEntrar;
+        private System.Windows.Forms.Button buttonSair;
+    }
+}
+
